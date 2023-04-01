a91c39dd8b2812a658a152d8c8eec5b27617664b
hussamahalshawi <hussamahalshawi@gmail.com>
2023-04-01 20:13:15 +0300

    edit

--- Clothes_Online_Store/settings.py
+++ Clothes_Online_Store/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$nr=**-=nvblqba_q89+u3f2i#((lu()7)-!eok%1hci-y##ru"
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
